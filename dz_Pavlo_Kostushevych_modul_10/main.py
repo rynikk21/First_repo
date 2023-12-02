1c68a58b895bff8241c653be8a44754a7d357f54
rynikk21 <kpp.en.ua@gmail.com>
2023-12-02 18:42:32 +0100

    modul_10

---
+++ dz_Pavlo_Kostushevych_modul_10/main.py
@@ -0,0 +1,34 @@
+class Mammal:
+    phrase = ''
+    def voice(self):
+        return self.phrase
+
+
+class Dog(Mammal):
+    phrase = 'Bark!'
+
+
+
+class Cat(Mammal):
+    phrase = 'Meow!'
+
+
+class Chupakabra:
+    def voice(self):
+        return 'Whooooo!!!'
+
+
+class Recorder:
+    def record_animal(self, animal):
+        voice = animal.voice()
+        print(f'Recorded "{voice}"')
+
+
+r = Recorder()
+cat = Cat()
+dog = Dog()
+strange_animal = Chupakabra()
+
+r.record_animal(cat)            # Recorded "Meow!"
+r.record_animal(dog)            # Recorded "Bark!"
+r.record_animal(strange_animal) # Recorded "Whooooo!!!"
